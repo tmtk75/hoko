d9f8cf3b877081d0ed9f8904eb9981f70be3254c
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-07-21 22:50:59 +0900

    logging

--- main.go
+++ main.go
@@ -42,18 +42,21 @@ func Run() {
 func ExecSerf(r render.Render, req *http.Request, params martini.Params) {
 	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
+		log.Printf("ioutil.ReadAll failed: %v", req.Body)
 		r.Error(400)
 		return
 	}
 
 	var body WebhookBody
 	if err := json.Unmarshal(b, &body); err != nil {
+		log.Printf("json.Unmarshal failed: %v", b)
 		r.Error(400)
 		return
 	}
 	body.Event = req.Header.Get("x-github-event")
 	payload, err := json.Marshal(&body)
 	if err != nil {
+		log.Printf("json.Marshal failed: %v", body)
 		r.Error(500)
 		return
 	}
@@ -64,8 +67,8 @@ func ExecSerf(r render.Render, req *http.Request, params martini.Params) {
 	q := command.QueryCommand{c, ui}
 	status := q.Run([]string{"-tag", "webhook=.*", "-format", "json", params["name"], string(payload)})
 
-	log.Printf("status: %v\n", status)
 	if status == 1 {
+		log.Printf("status: %v", status)
 		r.Data(500, buf.Bytes())
 	} else {
 		r.Data(200, buf.Bytes())
