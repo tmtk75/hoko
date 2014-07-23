c05b4992d3ec2809eff4b77fc690fc4c9cb29489
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-07-23 09:41:25 +0900

    test for verify x-hub-signature

--- mac_test.go
+++ mac_test.go
@@ -12,39 +12,25 @@ import (
 var body, key, sign []byte
 
 func init() {
-	fbody, _ := os.Open("reqbody.json")
-	fsec, _ := os.Open("secret_token.txt")
-	fsig, _ := os.Open("x-hub-signature.txt")
-	defer fbody.Close()
-	defer fsec.Close()
-	defer fsig.Close()
-	body, _ = ioutil.ReadAll(fbody)
-	key, _ = ioutil.ReadAll(fsec)
-	sign, _ = ioutil.ReadAll(fsig)
-}
-
-func Test_x_hub_signature1(t *testing.T) {
-	expected := sign[4+1 : len(sign)]
-
-	mac := hmac.New(sha1.New, key)
-	mac.Write(body)
-	actualBytes := mac.Sum(nil)
-	actualStr := hex.EncodeToString(actualBytes)
-	actual := []byte(actualBytes)
-
-	if actualStr != string(expected) {
-		t.Errorf("%v != %v\n", actual, expected)
+	open := func(name string) []byte {
+		f, _ := os.Open(name)
+		defer f.Close()
+		b, _ := ioutil.ReadAll(f)
+		return b
 	}
+	body = open("test/webhook-body.json")
+	key = open("test/secret_token.txt")
+	sign = open("test/x-hub-signature.txt")
 }
 
-func Test_x_hub_signature2(t *testing.T) {
+func Test_x_hub_signature(t *testing.T) {
 	expected, _ := hex.DecodeString(string(sign[4+1 : len(sign)]))
 
 	mac := hmac.New(sha1.New, key)
 	mac.Write(body)
-	actualBytes := mac.Sum(nil)
+	actual := mac.Sum(nil)
 
-	if !hmac.Equal(actualBytes, expected) {
-		t.Errorf("%v != %v\n", actualBytes, expected)
+	if !hmac.Equal(actual, expected) {
+		t.Errorf("%v != %v\n", actual, expected)
 	}
 }
