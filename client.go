99d6e0925a4df76946f9b538446e7fc4c78051f7
Tomotaka Sakuma <ktmtmks@gmail.com>
2014-07-24 10:00:40 +0900

    a simple http client to send a request with x-hug-signature header

---
+++ client.go
@@ -0,0 +1,57 @@
+//
+// usage:
+//   e.g) echo "{}" | SECRET_TOKEN=$(cat test/secret_token.txt) go run client.go
+//
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	secret := os.Getenv("SECRET_TOKEN")
+	if len(secret) == 0 {
+		log.Fatalf("SECRET_TOKEN is empty")
+	}
+
+	//log.Printf("secret: %v\n", secret)
+	key := []byte(secret)
+	body, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	//log.Printf("body: %v", body)
+
+	mac := hmac.New(sha1.New, key)
+	mac.Write(body)
+	actual := mac.Sum(nil)
+
+	sign := fmt.Sprintf("sha1=%s", hex.EncodeToString(actual))
+
+	req, err := http.NewRequest("POST", "http://localhost:3000/serf/query/github", bytes.NewReader(body))
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	req.Header.Add("x-hub-signature", sign)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	b, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	fmt.Printf("%v\n", res.Status)
+	fmt.Printf("%v\n", string(b))
+}
