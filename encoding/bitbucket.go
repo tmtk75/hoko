773c4e95da5366c7fbba38b5ce639a81f8746a4e
Tomotaka Sakuma <ktmtmks@gmail.com>
2016-11-05 16:14:14 +0900

    -

---
+++ encoding/bitbucket.go
@@ -0,0 +1,105 @@
+package encoding
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/codegangsta/cli"
+	"github.com/martini-contrib/render"
+)
+
+func UnmarshalBitbucket(payload []byte, ctx *cli.Context, r render.Render, req *http.Request, w http.ResponseWriter) interface{} {
+	if !ctx.Bool("d") {
+		token := os.Getenv(ENV_SECRET_TOKEN)
+		secret := req.URL.Query().Get("secret")
+		if token != secret {
+			log.Printf("secret token didn't match")
+			r.Error(400)
+			return nil
+		}
+	}
+
+	var body BitbucketWebhookBody
+	if err := json.Unmarshal(payload, &body); err != nil {
+		log.Printf("json.Unmarshal failed: %v", payload)
+		r.Error(400)
+		return nil
+	}
+
+	if _, ok := body.Push["changes"]; !ok {
+		log.Printf("push.changes is missing: %v", body)
+		r.Error(400)
+		return nil
+	}
+	if len(body.Push["changes"]) == 0 {
+		log.Printf("push.changes is empty: %v", body)
+		r.Error(400)
+		return nil
+	}
+
+	var wb WebhookBody
+	ch := body.Push["changes"][0]
+	switch ch.New.Type {
+	case "branch":
+		wb.Ref = "refs/heads/" + ch.New.Name
+	case "tag":
+		wb.Ref = "refs/tags/" + ch.New.Name
+	default:
+		log.Printf("unknown type: %v", ch.New.Type)
+		r.Error(400)
+		return nil
+	}
+	wb.Repository.Name = body.Repository.Name
+	wb.Repository.Owner.Name = body.Repository.Owner.Username
+	wb.Pusher.Name = body.Actor.Username
+	wb.Event = req.Header.Get("X-Event-Key")
+	wb.Created = ch.Created
+	wb.Deleted = ch.Truncated
+	wb.Delivery = req.Header.Get("X-Hook-UUID")
+	wb.After = ch.New.Target.Hash
+	wb.Before = ch.Old.Target.Hash
+	log.Printf("X-Request-UUID: %v", req.Header.Get("X-Request-UUID"))
+	log.Printf("X-Hook-UUID: %v", req.Header.Get("X-Hook-UUID"))
+	//log.Printf("%v", body)
+
+	if ctx.Bool("ignore-deleted") && ch.Truncated {
+		log.Printf("ignore truncated")
+		r.Header().Add("content-type", "text/plain")
+		r.Data(200, []byte("ignore truncated\n"))
+		return nil
+	}
+
+	return &wb
+}
+
+//https://confluence.atlassian.com/bitbucket/manage-webhooks-735643732.html
+type BitbucketWebhookBody struct {
+	Actor struct {
+		Username string `json:"username"`
+	} `json:"actor"`
+	Repository struct {
+		Name  string `json:"name"`
+		Owner struct {
+			Type     string `json:type`
+			Username string `json:username`
+		} `json:"owner"`
+	} `json:"repository"`
+	Push map[string][]struct { // changes
+		New struct {
+			Name   string `json:"name"`
+			Type   string `json:"type"`
+			Target struct {
+				Hash string `json:"hash"`
+			} `json:"target"`
+		} `json:"new"`
+		Old struct {
+			Target struct {
+				Hash string `json:"hash"`
+			} `json:"target"`
+		} `json:"old"`
+		Created   bool `json:"created"`
+		Truncated bool `json:"truncated"`
+	} `json:"push"`
+}
--- main.go
+++ main.go
@@ -129,70 +129,6 @@ type SerfCmd interface {
 	Run(args []string) int
 }
 
-func unmarshalBitbucket(payload []byte, ctx *cli.Context, r render.Render, req *http.Request, w http.ResponseWriter) interface{} {
-	if !ctx.Bool("d") {
-		token := os.Getenv(encoding.ENV_SECRET_TOKEN)
-		secret := req.URL.Query().Get("secret")
-		if token != secret {
-			log.Printf("secret token didn't match")
-			r.Error(400)
-			return nil
-		}
-	}
-
-	var body BitbucketWebhookBody
-	if err := json.Unmarshal(payload, &body); err != nil {
-		log.Printf("json.Unmarshal failed: %v", payload)
-		r.Error(400)
-		return nil
-	}
-
-	if _, ok := body.Push["changes"]; !ok {
-		log.Printf("push.changes is missing: %v", body)
-		r.Error(400)
-		return nil
-	}
-	if len(body.Push["changes"]) == 0 {
-		log.Printf("push.changes is empty: %v", body)
-		r.Error(400)
-		return nil
-	}
-
-	var wb encoding.WebhookBody
-	ch := body.Push["changes"][0]
-	switch ch.New.Type {
-	case "branch":
-		wb.Ref = "refs/heads/" + ch.New.Name
-	case "tag":
-		wb.Ref = "refs/tags/" + ch.New.Name
-	default:
-		log.Printf("unknown type: %v", ch.New.Type)
-		r.Error(400)
-		return nil
-	}
-	wb.Repository.Name = body.Repository.Name
-	wb.Repository.Owner.Name = body.Repository.Owner.Username
-	wb.Pusher.Name = body.Actor.Username
-	wb.Event = req.Header.Get("X-Event-Key")
-	wb.Created = ch.Created
-	wb.Deleted = ch.Truncated
-	wb.Delivery = req.Header.Get("X-Hook-UUID")
-	wb.After = ch.New.Target.Hash
-	wb.Before = ch.Old.Target.Hash
-	log.Printf("X-Request-UUID: %v", req.Header.Get("X-Request-UUID"))
-	log.Printf("X-Hook-UUID: %v", req.Header.Get("X-Hook-UUID"))
-	//log.Printf("%v", body)
-
-	if ctx.Bool("ignore-deleted") && ch.Truncated {
-		log.Printf("ignore truncated")
-		r.Header().Add("content-type", "text/plain")
-		r.Data(200, []byte("ignore truncated\n"))
-		return nil
-	}
-
-	return &wb
-}
-
 func ExecSerf(ctx *cli.Context, r render.Render, req *http.Request, params martini.Params, w http.ResponseWriter) {
 	b, err := ioutil.ReadAll(req.Body)
 	if err != nil {
@@ -210,7 +146,7 @@ func ExecSerf(ctx *cli.Context, r render.Render, req *http.Request, params marti
 	log.Printf("origin: %v\n", origin)
 	if origin == "bitbucket" {
 		os.Setenv(HOKO_ORIGIN, "bitbucket")
-		v = unmarshalBitbucket(b, ctx, r, req, w)
+		v = encoding.UnmarshalBitbucket(b, ctx, r, req, w)
 	} else {
 		os.Setenv(HOKO_ORIGIN, "github")
 		v = encoding.UnmarshalGithub(b, ctx, r, req, w)
@@ -271,34 +207,4 @@ func buildTagOptions(params map[string][]string) []string {
 		i += 1
 	}
 	return a
-}
-
-//https://confluence.atlassian.com/bitbucket/manage-webhooks-735643732.html
-type BitbucketWebhookBody struct {
-	Actor struct {
-		Username string `json:"username"`
-	} `json:"actor"`
-	Repository struct {
-		Name  string `json:"name"`
-		Owner struct {
-			Type     string `json:type`
-			Username string `json:username`
-		} `json:"owner"`
-	} `json:"repository"`
-	Push map[string][]struct { // changes
-		New struct {
-			Name   string `json:"name"`
-			Type   string `json:"type"`
-			Target struct {
-				Hash string `json:"hash"`
-			} `json:"target"`
-		} `json:"new"`
-		Old struct {
-			Target struct {
-				Hash string `json:"hash"`
-			} `json:"target"`
-		} `json:"old"`
-		Created   bool `json:"created"`
-		Truncated bool `json:"truncated"`
-	} `json:"push"`
 }
