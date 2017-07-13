f62789c28a103b59a2279f6d60fb751c901fa849
Tomotaka Sakuma <ktmtmks@gmail.com>
2017-07-13 23:03:40 +0900

    -

--- main.go
+++ main.go
@@ -12,8 +12,8 @@ import (
 
 	"github.com/codegangsta/cli"
 	"github.com/go-martini/martini"
-	"github.com/hashicorp/serf/cmd"
-	"github.com/hashicorp/serf/cmd/agent"
+	"github.com/hashicorp/serf/cmd/serf/command"
+	"github.com/hashicorp/serf/cmd/serf/command/agent"
 	"github.com/martini-contrib/render"
 
 	mcli "github.com/mitchellh/cli"
