3c9dff682f0a36f36316b8cbd72b034a759747a2
Tomotaka Sakuma <ktmtmks@gmail.com>
2019-06-14 20:37:06 +0900

    bump up version

--- main.go
+++ main.go
@@ -100,7 +100,7 @@ var commands = []cli.Command{
 func main() {
 	app := cli.NewApp()
 	app.Name = "hoko"
-	app.Version = "0.4.1"
+	app.Version = "0.4.3"
 	app.Usage = "A http server for github webhook with serf agent"
 	app.Commands = commands
 
